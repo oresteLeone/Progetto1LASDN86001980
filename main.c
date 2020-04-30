2d21c711f60db1249aa55c108c7512902577450c
OresteLeone <or.leone@studenti.unina.it>
2020-04-30 13:28:14 +0200

    creati appunti e primo approccio al menu

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 #include <string.h>
 
 int main(){
-    printf("\n Main \n");
+
+    printf("\n dovrei inizializzare i libri \n");
+
+    int choice=0;
+    printf("\n1. Aggiungere una richiesta ");
+    printf("\n2. Gestire le richieste ");
+    printf("\n3. Terminare ");
+    printf("\n input: ");
+    do{
+        scanf("%d",&choice);
+    }while(choice<0 && choice>4);
+    printf("\n ho selezionato %d",choice);
     return 0; 
 }
